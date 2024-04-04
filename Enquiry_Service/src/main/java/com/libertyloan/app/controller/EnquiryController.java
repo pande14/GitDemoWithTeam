2cc84f8aa6f277a7d98ed824d79dc7d9949f36e7
sjshi <sjshi@shweta>
2024-04-04 10:26:25 +0530

    shweta post apl

--- Enquiry_Service/src/main/java/com/libertyloan/app/controller/EnquiryController.java
+++ Enquiry_Service/src/main/java/com/libertyloan/app/controller/EnquiryController.java
@@ -1,21 +1,21 @@
 package com.libertyloan.app.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EnquiryController {
 
-//	@GetMapping("/getEnquires")
-//	public String getEnquiry()
-//	{
-//	  return "No Enquiry Found ..!";
-//		
-//	}
+	
 	@GetMapping("/getlogin")
 	public String login()
 	{
 		return "no enquiry found";
 	}
-	
+	@PostMapping("/postlogin")
+	public String loginCheck()
+	{
+		return "hdhggcvdge";
+	}
 }
