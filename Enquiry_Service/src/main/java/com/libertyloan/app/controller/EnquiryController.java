9dcd128728a2082daf31ebbff8728db19e697c8a
MadhuriShelke12 <97649711+MadhuriShelke12@users.noreply.github.com>
2024-04-04 10:40:37 +0530

    Madhuri APT

--- Enquiry_Service/src/main/java/com/libertyloan/app/controller/EnquiryController.java
+++ Enquiry_Service/src/main/java/com/libertyloan/app/controller/EnquiryController.java
@@ -2,6 +2,7 @@ package com.libertyloan.app.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,5 +18,10 @@ public class EnquiryController {
 	public String loginCheck()
 	{
 		return "hdhggcvdge";
+	}
+	@PutMapping("/update")
+	public String getData()
+	{
+		return "update APT";
 	}
 }
